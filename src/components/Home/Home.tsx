e985b65b57c6fb0a63577b1c114e026351b7fe2c
ruck-45 <thoughtsfirstborn01@gmail.com>
2024-02-10 00:40:04 +0530

    Home components complete

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -4,13 +4,20 @@ import { useDispatch } from "react-redux";
 // Local Files
 import { updateTab } from "../../store/curTabSlice";
 import { scrollTop } from "../../utils/controllers";
+import Hero from "./subComponents/Hero";
+import Track from "./subComponents/Track";
 
 const Home = () => {
   const dispatch = useDispatch();
   dispatch(updateTab("Home"));
   scrollTop();
 
-  return <div className="bg-black">Home</div>;
+  return (
+    <div>
+      <Hero />
+      <Track />
+    </div>
+  );
 };
 
 export default Home;
